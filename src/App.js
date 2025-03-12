47ce3d802abe7ba288086ab952dca14223ad86bb
gevoglanyan <159603975+gevoglanyan@users.noreply.github.com>
2025-03-11 18:13:35 -0700

    Updates (Coming Soon)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-Header">
+        <img src="Harold.jpg" alt="Harold" ></img>
+        <p>Gevoglanyan</p>
+        <p>Coming Soon!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
