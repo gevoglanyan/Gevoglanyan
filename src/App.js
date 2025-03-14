f390e21629899316788f0c188616891b6a9116eb
gevoglanyan <159603975+gevoglanyan@users.noreply.github.com>
2025-03-13 17:47:12 -0700

    Added iAqua & Footer with Styling

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ function App() {
   return (
     <>
       <div className="App">
+        <br />
+        <br />
         <header className="App-Header">
           <img src="Harold.jpg" alt="Harold" />
           <br />
-          <p>Harutyun</p>
-          <br />
           <p>Coming Soon!</p>
+          <br />
+          <p>
+            Visit <a href="https://iaquaaquatics.com" target="_blank" rel="noopener noreferrer">iAqua Aquatics</a>
+          </p>
         </header>
+
+        <div className="footer">
+          <p>&copy; 2025 Gevoglanyan. All Rights Reserved.</p>
+        </div>
       </div>
 
       <Analytics />
