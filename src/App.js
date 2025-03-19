0927fcc71bc6b67040afc9f4bd6e6fbc057757ff
gevoglanyan <159603975+gevoglanyan@users.noreply.github.com>
2025-03-19 15:53:49 -0700

    CSS Edits

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ function App() {
   return (
     <>
       <div className="App">
-        <br />
-        <br />
         <header className="App-Header">
           <img src="Harold.jpg" alt="Harold" />
+
           <br />
-          <p>Coming Soon!</p>
           <br />
+
           <p>
-            Visit <a href="https://iaquaaquatics.com" target="_blank" rel="noopener noreferrer">iAqua Aquatics</a>
+            <a href="https://iaquaaquatics.com" target="_blank" rel="noopener noreferrer">iAqua Aquatics</a>
           </p>
         </header>
 
+        <br />
+        <br />
+      
         <div className="footer">
           <p>&copy; 2025 Gevoglanyan. All Rights Reserved.</p>
         </div>
