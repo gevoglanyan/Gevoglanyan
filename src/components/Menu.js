af4738f8526aa7a6e690ea4a4351d4f8183d2fe3
Harutyun <gevoglanyan.harutyun@gmail.com>
2025-06-08 22:04:59 -0700

    Updated Mobile Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 
 const sections = [
-  { id: 'about', label: 'ABOUT' },
+  { id: 'landing', label: 'LANDING' },
+   { id: 'about', label: 'ABOUT' },
   { id: 'skills', label: 'SKILLS' },
   { id: 'projects', label: 'PROJECTS' },
   { id: 'gaming', label: 'GAMING' },
@@ -48,6 +49,8 @@ const MobileMenu = ({ onClose }) => {
           <div className="absolute right-0 top-0 aspect-square h-full w-2 bg-cyan-400 blur-[50px]" />
         </div>
 
+        < br /> 
+
         <footer className="relative mt-10">
           <div className="relative overflow-hidden pb-8">
             <hr className="absolute left-[-10%] h-2 w-[200vw] bg-white-400" />
@@ -59,15 +62,11 @@ const MobileMenu = ({ onClose }) => {
           </div>
           <section className="flex flex-col gap-4 items-center mt-8 text-sm text-white-400">
             <div> 
-              <br /> Designed & Powered
+              <br/><br /> Designed & Powered
             </div>
             <div>
               by Harutyun Gevoglanyan 
             </div>
-
-            <br /> 
-            
-            <a href="mailto:gevoglanyan.harutyun@gmail.com" className="text-white-400 underline">gevoglanyan.harutyun@gmail.com</a>
           </section>
         </footer>
       </div>
