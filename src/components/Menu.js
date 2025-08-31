d33a102cc6904eab02d52f0b53b9121af082f26c
gevoglanyan <159603975+gevoglanyan@users.noreply.github.com>
2025-08-31 00:12:30 -0700

    Fixed Mobile Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const sections = [
   { id: 'landing', label: 'LANDING' },
-   { id: 'about', label: 'ABOUT' },
+  { id: 'about', label: 'ABOUT' },
   { id: 'skills', label: 'SKILLS' },
   { id: 'projects', label: 'PROJECTS' },
   { id: 'gaming', label: 'GAMING' },
@@ -30,11 +30,11 @@ const MobileMenu = ({ onClose }) => {
   };
 
   return (
-    <aside className="flex md:hidden fixed inset-0 z-40 h-full w-full bg-black px-6 pt-[1vh]">
-      <div className="flex flex-col justify-center w-full">
+    <aside className="md:hidden fixed inset-0 z-40 bg-black px-6 pt-[1vh] overflow-y-auto">
+      <div className="flex flex-col min-h-screen w-full">
         <div className="relative w-fit mx-auto">
 
-          < br /> < br />
+          < br /> < br /> < br /> < br />
           
           <ul className="flex flex-col gap-6 uppercase text-2xl text-white">
             {sections.map((section, idx) => (
@@ -52,7 +52,7 @@ const MobileMenu = ({ onClose }) => {
           <div className="absolute right-0 top-0 aspect-square h-full w-2 bg-cyan-400 blur-[50px]" />
         </div>
 
-        < br /> 
+        < br /> < br /> 
 
         <footer className="relative mt-10">
           <div className="relative overflow-hidden pb-8">
@@ -60,18 +60,17 @@ const MobileMenu = ({ onClose }) => {
             <hr className="absolute left-[-10%] h-2 w-[200vw] bg-white-400 blur-[20px]" />
           </div>
           <div className="text-4xl font-bold text-center text-white mb-6">
-            < br /> Harutyun
             < br /> Gevoglanyan
           </div>
           <section className="flex flex-col gap-4 items-center mt-8 text-sm text-white-400">
             <div> 
-              <br/><br /> Designed & Powered
+              <br/>Designed & Powered
             </div>
             <div>
               by Harutyun Gevoglanyan 
             </div>
 
-            <br />
+            {/*
 
             <div className="flex flex-row gap-4 justify-center items-center mb-6">
               <a
@@ -110,6 +109,8 @@ const MobileMenu = ({ onClose }) => {
             </div>
 
             < br /> < br />
+
+            */}
           
           </section>
         </footer>
